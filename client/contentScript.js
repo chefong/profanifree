53dc95216cd0b56c0c4355203f0f2f0be6b23237
ericong18 <ericong18@gmail.com>
2020-02-01 12:12:46 -0800

    Fix morse code beeping

--- client/contentScript.js
+++ client/contentScript.js
@@ -1,36 +1,12 @@
-const BASE_URL = 'https://80802807.ngrok.io';
+const BASE_URL = 'https://2360b618.ngrok.io';
 const ytVideo = document.getElementsByClassName('video-stream')[0];
 const ytUrlIdSeparator = 'v=';
 const ytURL = window.location.href.split(ytUrlIdSeparator);
 const ytID = ytURL[1];
-var censorBeep = new Audio(chrome.runtime.getURL('censor-beep-4.mp3'));
-censorBeep.play();
-
-ytVideo.addEventListener("pause", event => {
-  console.log('paused audio', censorBeep.volume)
-  event.preventDefault();
-  if (!censorBeep.paused) {
-    censorBeep.pause();
-  }
-}, false);
-
-function muteVideo(video, timestamps, roundedTime) {
-  censorBeep.play();
-  censorBeep.volume = 0.2;
-
-  setTimeout(function() {
-    video.muted = false;
-    censorBeep.volume = 0;
-  }, timestamps.get(roundedTime) * 1000);
-}
-
-function cleanVideoSegment(video, timestamps) {
-  const roundedTime = Math.round(video.currentTime);
-  if (timestamps.has(roundedTime)) {
-    // video.currentTime = video.currentTime + timestamps.get(roundedTime);
-    muteVideo(video, timestamps, roundedTime);
-  }
-}
+let unmuteTimer;
+let videoTime;
+let censorBeep = new Audio(chrome.runtime.getURL('censor-beep-4.mp3'));
+censorBeep.volume = 0.25;
 
 function checkVideo() {
   fetch(`${BASE_URL}/`, {
@@ -49,15 +25,50 @@ function checkVideo() {
 
         offendingLines.forEach(line => {
           const [phrase, timestamp, durationAmount] = line;
-          const roundedTimestamp = Math.round(timestamp);
+          const roundedTimestamp = Math.round(timestamp) + 2;
           const roundedDurationAmount = Math.round(durationAmount) + 1;
           timestamps.set(roundedTimestamp, roundedDurationAmount);
         });
 
-        setInterval(cleanVideoSegment, 100, ytVideo, timestamps);
+        videoTime = setInterval(function() {
+          let roundedTime = Math.round(ytVideo.currentTime);
+          console.log(roundedTime);
+          if (timestamps.has(roundedTime)) {
+            ytVideo.muted = true;
+            censorBeep.play();
+            unmuteTimer = setTimeout(function() {
+              ytVideo.muted = false;
+              censorBeep.pause();
+              censorBeep.currentTime = 0;
+            }, 2000);
+          }
+        }, 100);
+
+        ytVideo.addEventListener('play', () => {
+          videoTime = setInterval(function() {
+            let roundedTime = Math.round(ytVideo.currentTime);
+            console.log(roundedTime);
+            if (timestamps.has(roundedTime)) {
+              ytVideo.muted = true;
+              censorBeep.play();
+              unmuteTimer = setTimeout(function() {
+                ytVideo.muted = false;
+                censorBeep.pause();
+                censorBeep.currentTime = 0;
+              }, 2000);
+            }
+          }, 100);
+        })
       }
     })
     .catch(error => console.error('###########      ERROR', error));
 }
 
 window.addEventListener("load", checkVideo, false);
+
+ytVideo.addEventListener('pause', () => {
+  console.log('pause EVENT LISTENER FROM VIDEO')
+  clearTimeout(unmuteTimer);
+  clearInterval(videoTime);
+  console.log("hewwo")
+});
