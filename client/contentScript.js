737b01ba566c0c5c62021dcd63f598ed7ec77468
ericong18 <ericong18@gmail.com>
2020-02-01 00:34:03 -0800

    Use given timestamp durations to fast forward

--- client/contentScript.js
+++ client/contentScript.js
@@ -4,38 +4,41 @@ const ytUrlIdSeparator = 'v=';
 const ytURL = window.location.href.split(ytUrlIdSeparator);
 const ytID = ytURL[1];
 
-// chrome.runtime.sendMessage(
-//   {
-//       contentScriptQuery: "postData"
-//       , data: 'hi'
-//       , url: BASE_URL
-//   }, function (response) {
-//       debugger;
-//       if (response != undefined && response != "") {
-//           callback(response);
-//       }
-//       else {
-//           debugger;
-//           callback(null);
-//       }
-//   });
-
-fetch(`${BASE_URL}/`, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({ video_id: ytID })
-})
-.then(response => response.json())
-.then(data => console.log('data', data))
+function fastForward(video, timestamps, currentTime) {
+  if (timestamps.has(currentTime)) {
+    video.currentTime = video.currentTime + timestamps.get(currentTime);
+  }
+}
 
 function checkVideo() {
-  // setInterval(function() {
-  //   if (Math.round(ytVideo.currentTime) % 10 === 0){
-  //     ytVideo.currentTime = ytVideo.currentTime + 5;
-  //   }
-  // }, 100);
+  fetch(`${BASE_URL}/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ video_id: ytID })
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      const { curse_words: hasCurseWords, offending_lines: offendingLines } = data;
+      if (hasCurseWords) {
+        const timestamps = new Map();
+
+        offendingLines.forEach(line => {
+          const [phrase, timestamp, durationAmount] = line;
+          timestamps.set(Math.round(timestamp), Math.round(durationAmount));
+        });
+
+        setInterval(function() {
+          const roundedTime = Math.round(ytVideo.currentTime);
+          if (timestamps.has(roundedTime)) {
+            ytVideo.currentTime = ytVideo.currentTime + timestamps.get(roundedTime);
+          }
+        }, 100);
+      }
+    })
+    .catch(error => console.error('       ###########      ERROR', error));
 }
 
 window.addEventListener("load", checkVideo, false);
